51ce1344677390218b07053840bf6568c6ffb5cb
foyfox58 <foy9323@gmail.com>
2026-03-26 20:16:06 +0700

    feat: Set up Vue Router, introduce a Design System page demonstrating various badge components, and configure TypeScript path aliases.

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,15 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 // เดี๋ยวเราค่อยมา import หน้า views ต่างๆ ทีหลัง
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    // ตัวอย่างการตั้งค่า Route หน้า Landing Page
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: () => import('../views/HomeView.vue')
-    // }
-  ]
-})
-
-export default router
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+      {
+        path: '/', // เมื่อเข้า URL หลัก (localhost:5173)
+        name: 'design-system',
+        component: () => import('../views/DesignSystem.vue') // ให้ดึงไฟล์นี้มาแสดง
+      }
+    ]
+  })
+  
+  export default router
